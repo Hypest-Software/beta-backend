34d9ec34314766d639e9f32f90eda30e8aa6d410
congminh1254 <clovers1254@gmail.com>
2020-11-21 17:57:42 +0100

    API Update Information

---
+++ beta_apis/jwt_utils.py
@@ -0,0 +1,60 @@
+"""Methods related to jwt, including encode, decode"""
+import jwt
+from base64 import b64decode
+from django.conf import settings
+import time
+from random import randint
+
+
+def decode_jwt(token):
+    """Decodes the jwt token with ecrypted key from settings
+    Returns the dict of data
+    """
+    # Removes some extra text
+    token = token.replace('Bearer ', '')
+    token = token.replace('Token ', '')
+    try:
+        result = jwt.decode(token, b64decode(settings.JWT_ENCRYPT_KEY))
+    except jwt.exceptions.DecodeError:
+        result = None
+    return result
+
+
+def encode_jwt(user_id, algorithm='HS256', exp_in_seconds=864000):
+    """Create a jwt token with user_id. What is the jti?
+    """
+
+    iat = int(time.time())
+    exp = iat + exp_in_seconds
+    jti = iat + randint(9999, 999999)
+    data = dict(jti=jti, sub=user_id, iat=iat, exp=exp)
+    return jwt.encode(data, b64decode(settings.JWT_ENCRYPT_KEY), algorithm=algorithm)
+
+
+def detect_lang(lang_header):
+    lang = 'en'  # or vi
+    if 'vi' in lang_header:
+        lang = 'vi'
+    return lang
+
+
+def jwt_decode(token):
+    return jwt.decode(token, b64decode(settings.JWT_ENCRYPT_KEY))
+
+
+def generate_blog_api_token():
+    iat = int(time.time())
+    exp = iat + 31556926 * 10
+    iss = settings.BLOG_JOBHOP_URL
+    data = dict(
+        iss=iss,
+        iat=iat,
+        nbf=iat,
+        exp=exp,
+        data=dict(
+            user=dict(
+                id=2
+            )
+        )
+    )
+    return jwt.encode(data, settings.BLOG_JWT_AUTH_SECRET_KEY)
--- beta_backend/settings.py
+++ beta_backend/settings.py
@@ -126,49 +126,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+JWT_ENCRYPT_KEY = env('JWT_ENCRYPT_KEY')
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'beta_apis.authencation.JWTAuthentication',
     ),
-}
-
-JWT_AUTH = {
-    'JWT_ENCODE_HANDLER':
-    'rest_framework_jwt.utils.jwt_encode_handler',
-
-    'JWT_DECODE_HANDLER':
-    'rest_framework_jwt.utils.jwt_decode_handler',
-
-    'JWT_PAYLOAD_HANDLER':
-    'rest_framework_jwt.utils.jwt_payload_handler',
-
-    'JWT_PAYLOAD_GET_USER_ID_HANDLER':
-    'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
-
-    'JWT_RESPONSE_PAYLOAD_HANDLER':
-    'rest_framework_jwt.utils.jwt_response_payload_handler',
-
-    'JWT_SECRET_KEY': env('SECRET_KEY'),
-    'JWT_GET_USER_SECRET_KEY': env('SECRET_KEY'),
-    'JWT_PUBLIC_KEY': env('SECRET_KEY'),
-    'JWT_PRIVATE_KEY': env('SECRET_KEY'),
-    'JWT_ALGORITHM': 'HS256',
-    'JWT_VERIFY': True,
-    'JWT_VERIFY_EXPIRATION': True,
-    'JWT_LEEWAY': 0,
-    'JWT_EXPIRATION_DELTA': timedelta(days=30),
-    'JWT_AUDIENCE': None,
-    'JWT_ISSUER': None,
-
-    'JWT_ALLOW_REFRESH': False,
-    'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=30),
-
-    'JWT_AUTH_HEADER_PREFIX': 'JWT',
-    'JWT_AUTH_COOKIE': None,
-
 }
 
 
