6c6882135280bd17fe29dfe0c90b2157a8328e83
congminh1254 <clovers1254@gmail.com>
2020-11-27 23:17:20 +0100

    Update response body

--- beta_apis/constants.py
+++ beta_apis/constants.py
@@ -2,37 +2,17 @@ from rest_framework.response import Response
 from rest_framework import serializers
 
 class FailedResponse(Response):
-    response_data = {
-        "success": False,
-        "statusCode": 400,
-        "statusMessage": "",
-        "data": None
-    }
 
-    def __init__(self, data=None, status_code=400, status_message='', **kwargs):
-        response_data = self.response_data
-        response_data['statusMessage'] = status_message
-        response_data['statusCode'] = status_code
-        response_data['data'] = data
-        data = response_data
-        super().__init__(data, **kwargs)
+    def __init__(self, data=dict(), status_code=400, status_message='', **kwargs):
+        data['message'] = status_message
+        super().__init__(data, status=status_code,  **kwargs)
 
 
 class SuccessResponse(Response):
-    response_data = {
-        "success": True,
-        "statusCode": 0,
-        "statusMessage": "Success",
-        "data": None
-    }
 
     def __init__(self, data=None, status_code=200, status_message='Success', **kwargs):
-        response_data = self.response_data
-        response_data['statusMessage'] = status_message
-        response_data['statusCode'] = status_code
-        response_data['data'] = data
-        data = response_data
-        super().__init__(data, **kwargs)
+        data['message'] = status_message
+        super().__init__(data, status=status_code, **kwargs)
 
 
 class RawResponse(Response):
